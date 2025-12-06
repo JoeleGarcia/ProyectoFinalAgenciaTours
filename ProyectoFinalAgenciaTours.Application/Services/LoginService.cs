0faf15271d4e34ee7c96c3011384de781c3bf392
JoelGarcia <Joel.eGarcia@live.com>
2025-12-06 00:42:52 -0400

    Update: Logica para LoginService y PaisService

--- ProyectoFinalAgenciaTours.Application/Services/LoginService.cs
+++ ProyectoFinalAgenciaTours.Application/Services/LoginService.cs
@@ -1,5 +1,10 @@
-﻿using ProyectoFinalAgenciaTours.Application.DTOs;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using ProyectoFinalAgenciaTours.Application.DTOs;
+using ProyectoFinalAgenciaTours.Application.Exceptions;
 using ProyectoFinalAgenciaTours.Application.Interfaces;
+using ProyectoFinalAgenciaTours.Domain.Entities;
+using ProyectoFinalAgenciaTours.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +15,24 @@ namespace ProyectoFinalAgenciaTours.Application.Services
 {
     public class LoginService : ILoginService
     {
+        private readonly IUserRepository _userRepository;
+        private readonly ISessionManager _sessionManagerRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMapper _mapper;
+
+        public LoginService(
+                IUserRepository userRepository
+            ,   ISessionManager sessionManagerRepository
+            ,   IHttpContextAccessor httpContextAccessor
+            ,   IMapper mapper
+        )
+        {
+            _userRepository = userRepository;
+            _sessionManagerRepository = sessionManagerRepository;
+            _httpContextAccessor = httpContextAccessor;
+            _mapper = mapper;
+        }
+
         public string GetUserId()
         {
             throw new NotImplementedException();
@@ -20,9 +43,18 @@ namespace ProyectoFinalAgenciaTours.Application.Services
             throw new NotImplementedException();
         }
 
-        public Task<UsuarioDto> LoginAsync(string email, string password)
+        public async Task<UsuarioDto> LoginAsync(string username, string password)
         {
-            throw new NotImplementedException();
+            var response = await _userRepository.ValidateCredentialsAsync(username, password);
+
+            if (response is null)
+            {
+                return null!;
+            }
+
+            await _sessionManagerRepository.SignInAsync(response);
+
+            return new UsuarioDto(response.Id, response.Nombre, response.Apellido, response.Username, response.Email, response.Role, response.Status);
         }
 
         public Task<LoginJwtResponseDto> LoginForJwtAsync(LoginUsuarioDto loginUsuarioDto)
@@ -30,14 +62,32 @@ namespace ProyectoFinalAgenciaTours.Application.Services
             throw new NotImplementedException();
         }
 
-        public Task LogoutAsync()
+        public async Task LogoutAsync()
         {
-            throw new NotImplementedException();
+            await _sessionManagerRepository.SignOutAsync();
         }
 
-        public Task<bool> RegisterAsync(RegistroUsuarioDto registroUsuarioDto)
+        public async Task<bool> RegisterAsync(RegistroUsuarioDto registroUsuarioDto)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var user = new Usuario
+                {
+                    Username = registroUsuarioDto.Username,
+                    Password = registroUsuarioDto.Password,
+                    Email = registroUsuarioDto.Email,
+                    Nombre = registroUsuarioDto.Nombre,
+                    Apellido = registroUsuarioDto.Apellido
+                };
+
+                var userRegistered = await _userRepository.AddUserAsync(user);
+
+                return userRegistered;
+            }
+            catch (InfrastructureException ex)
+            {
+                throw new ApplicationException("No se pudo registrar el usuario. Intente más tarde.", ex);
+            }
         }
 
         public Task<UsuarioDto> UserInfo(string id)
---
+++ ProyectoFinalAgenciaTours.Application/Services/PaisService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using ProyectoFinalAgenciaTours.Application.DTOs;
+using ProyectoFinalAgenciaTours.Application.Exceptions;
+using ProyectoFinalAgenciaTours.Application.Interfaces;
+using ProyectoFinalAgenciaTours.Domain.Entities;
+using ProyectoFinalAgenciaTours.Domain.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProyectoFinalAgenciaTours.Application.Services
+{
+    public class PaisService : IPaisService
+    {
+        private readonly IPaisRepository _paisRepository;
+        private readonly IMapper _mapper;
+
+        public PaisService(IPaisRepository paisRepository, IMapper mapper)
+        {
+            _paisRepository = paisRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatePaisDto> AddPaisAsync(CreatePaisDto createPaisDto)
+        {
+            try
+            {
+                var _materia = _mapper.Map<Pais>(createPaisDto);
+                var materiaAgregado = await _paisRepository.AddPaisAsync(_materia);
+
+                var CreatePaisDto = _mapper.Map<CreatePaisDto>(materiaAgregado);
+                
+                return CreatePaisDto;
+
+            }
+            catch (InfrastructureException ex)
+            {
+                throw new ApplicationException("No se pudo registrar el usuario. Intente más tarde.", ex);
+            }
+        }
+
+        public async Task DeletePaisAsync(int id)
+        {
+            try
+            {
+                await _paisRepository.DeletePaisAsync(id);
+            }
+            catch (InfrastructureException ex)
+            {
+                throw new InfrastructureException("Timeout al consultar la base de datos", ex);
+            }
+            catch (NotFoundException ex)
+            {
+                throw new NotFoundException("Timeout al consultar la base de datos", ex);
+
+            }
+            catch (Exception ex)
+            {
+                throw new NotFoundException("Pais", ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<PaisDto>> GetAllPaisAsync()
+        {
+            var paises = await _paisRepository.GetAllPaisAsync();
+            return _mapper.Map<IEnumerable<PaisDto>>(paises);
+        }
+
+        public async Task<PaisDto> GetPaisByIdAsync(int id)
+        {
+            var _pais = await _paisRepository.GetPaisByIdAsync(id);
+            return _mapper.Map<PaisDto>(_pais);
+        }
+
+        public async Task UpdatePaisAsync(int id, UpdatePaisDto updatePaisDto)
+        {
+            var _pais = await _paisRepository.GetPaisByIdAsync(id);
+
+            if (_pais is null)
+            {
+                throw new NotFoundException("Pais", id);
+            }
+
+            var updatedPais = _mapper.Map<Pais>(updatePaisDto);        
+
+            await _paisRepository.UpdatePaisAsync(updatedPais);
+        }
+    }
+}
