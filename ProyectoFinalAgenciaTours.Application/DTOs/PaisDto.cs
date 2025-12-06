9ddeab35627b867ab08030de566058806b18d013
JoelGarcia <Joel.eGarcia@live.com>
2025-12-06 00:39:46 -0400

    Update: Dto Pais

--- ProyectoFinalAgenciaTours.Application/DTOs/PaisDto.cs
+++ ProyectoFinalAgenciaTours.Application/DTOs/PaisDto.cs
@@ -1,7 +1,19 @@
 ﻿namespace ProyectoFinalAgenciaTours.Application.DTOs
 {
-    public record PaisDto(int Id, string Nombre, string CodigoISO, string Moneda, string ZonaHorariaBase, bool RequiereVisa, DateTime FechaCreacion);
-    public record CreatePaisDto(string Nombre, string CodigoISO, string Moneda, string ZonaHorariaBase, bool RequiereVisa);
-    public record UpdatePaisDto(int Id, string Nombre, string CodigoISO, string Moneda, string ZonaHorariaBase, bool RequiereVisa);
+    public record PaisDto
+    {
+        public int Id { get; init; }
+        public string Nombre { get; init; }
+        public string CodigoISO { get; init; }
+        public string Moneda { get; init; }
+        public string IdiomaOficial { get; init; }
+        public bool RequiereVisa { get; init; }
+        public bool Status { get; init; }
+        public DateTime FechaCreacion { get; init; }
+        public DateTime? FechaModificacion { get; init; }
+    }
+
+    public record CreatePaisDto(string Nombre, string CodigoISO, string Moneda, string IdiomaOficial, bool RequiereVisa, bool Status);
+    public record UpdatePaisDto(int Id, string Nombre, string CodigoISO, string Moneda,  string IdiomaOficial, bool RequiereVisa, bool Status);
 
 }
