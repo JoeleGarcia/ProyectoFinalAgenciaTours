fe1a8cdb6892af726d684ba4687697368d58a3cd
JoelGarcia <Joel.eGarcia@live.com>
2025-12-06 00:45:16 -0400

    Update: Nuevas Interfaces PaisService y UserRepository

---
+++ ProyectoFinalAgenciaTours.Domain/Interfaces/IPaisRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProyectoFinalAgenciaTours.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAgenciaTours.Domain.Interfaces
+{
+    public interface IPaisRepository
+    {
+        Task<IEnumerable<Pais>> GetAllPaisAsync();
+        Task<Pais?> GetPaisByIdAsync(int id);
+        Task<Pais> AddPaisAsync(Pais pais);
+        Task UpdatePaisAsync(Pais pais);
+        Task DeletePaisAsync(int id);
+    }
+}
---
+++ ProyectoFinalAgenciaTours.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProyectoFinalAgenciaTours.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAgenciaTours.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<bool> AddUserAsync(Usuario user);
+        Task<Usuario> GetUserByIdAsync(Guid id);
+        Task<Usuario?> GetByEmailAsync(string email);
+        Task<Usuario?> GetByUsernameAsync(string username);
+        Task<bool> IsEmailRegisteredAsync(string email);
+        Task<bool> IsUsernameRegisteredAsync(string username);
+        Task<Usuario> ValidateCredentialsAsync(string email, string password);
+    }
+}
