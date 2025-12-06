d24ec0dcdc91e49c571727e05e94b73b5c564b49
JoelGarcia <Joel.eGarcia@live.com>
2025-12-06 00:47:12 -0400

    Update: Se agrega las logicas para la capa de Infraestructura

---
+++ ProyectoFinalAgenciaTours.Infrastructure/Repositories/PaisRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAgenciaTours.Application.Exceptions;
+using ProyectoFinalAgenciaTours.Domain.Entities;
+using ProyectoFinalAgenciaTours.Domain.Interfaces;
+using ProyectoFinalAgenciaTours.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAgenciaTours.Infrastructure.Repositories
+{
+    public class PaisRepository : IPaisRepository
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public PaisRepository(ApplicationDbContext context)
+        {
+            _dbcontext = context;
+        }
+        public async Task<Pais> AddPaisAsync(Pais pais)
+        {
+            try
+            {
+                await _dbcontext.Paises.AddAsync(pais);
+                await _dbcontext.SaveChangesAsync();
+                return pais;
+            }
+            catch (SqlException ex) when (ex.Number == -2)
+            {
+                throw new InfrastructureException("Timeout al consultar la base de datos", ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InfrastructureException("Error en la consulta de Materia", ex);
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new InfrastructureException(ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new InfrastructureException("Error inesperado en la base de datos", ex);
+            }
+
+        }
+
+        public async Task DeletePaisAsync(int id)
+        {
+            var _pais = await GetPaisByIdAsync(id);
+
+            if (_pais is null)
+                throw new NotFoundException("Pais", string.Format("By Id {id}", id.ToString()));
+
+            _dbcontext.Paises.Remove(_pais);
+            await _dbcontext.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Pais>> GetAllPaisAsync()
+        {
+            return await _dbcontext.Paises.ToListAsync();
+
+        }
+
+        public async Task<Pais?> GetPaisByIdAsync(int id)
+        {
+            return await _dbcontext.Paises.AsNoTracking()
+                                            .FirstOrDefaultAsync(p => p.Id == id); ;
+        }
+
+        public async Task UpdatePaisAsync(Pais pais)
+        {
+            _dbcontext.Paises.Update(pais);
+            await _dbcontext.SaveChangesAsync();
+        }
+    }
+}
---
+++ ProyectoFinalAgenciaTours.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinalAgenciaTours.Infrastructure.Data;
+using ProyectoFinalAgenciaTours.Application.Exceptions;
+using ProyectoFinalAgenciaTours.Domain.Entities;
+using ProyectoFinalAgenciaTours.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAgenciaTours.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly ApplicationDbContext _dbcontext;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _dbcontext = context;
+        }
+
+        public Task<bool> AddUserAsync(Usuario user)
+        {
+            try
+            {
+                _dbcontext.Usuarios.Add(user);
+                var result = _dbcontext.SaveChangesAsync();
+                return Task.FromResult(result.Result > 0);
+            }
+            catch (SqlException ex) when (ex.Number == -2) // Timeout específico
+            {
+                throw new InfrastructureException("Timeout al consultar la base de datos", ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InfrastructureException("Error en la consulta de usuario", ex);
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new InfrastructureException(ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                throw new InfrastructureException("Error inesperado en la base de datos", ex);
+            }
+        }
+
+        public async Task<Usuario?> GetByEmailAsync(string email)
+        {
+            return await _dbcontext.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<Usuario> GetUserByIdAsync(Guid id)
+        {
+            try
+            {
+                var user = await _dbcontext.Usuarios.FirstOrDefaultAsync(u => u.Id.Equals(id));
+
+                if (user == null)
+                    return null!;
+
+                return user;
+            }
+            catch (SqlException ex) when (ex.Number == -2) // Timeout específico
+            {
+                throw new InfrastructureException("Timeout al consultar la base de datos", ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InfrastructureException("Error en la consulta de usuario", ex);
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new InfrastructureException("Error en la base de datos", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new InfrastructureException("Error inesperado en la base de datos", ex);
+            }
+
+        }
+
+        public Task<Usuario?> GetByUsernameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsEmailRegisteredAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsUsernameRegisteredAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Usuario> ValidateCredentialsAsync(string email, string password)
+        {
+            try
+            {
+                var user = await _dbcontext.Usuarios.FirstOrDefaultAsync(u => u.Email == email && u.Password.Equals(password));
+
+                if (user == null)
+                    return null!; // usuario no existe
+
+                return user; // login correcto
+            }
+            catch (SqlException ex) when (ex.Number == -2) // Timeout específico
+            {
+                throw new InfrastructureException("Timeout al consultar la base de datos", ex);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new InfrastructureException("Error en la consulta de usuario", ex);
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new InfrastructureException("Error en la base de datos", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new InfrastructureException("Error inesperado en la base de datos", ex);
+            }
+
+        }
+
+    }
+}
