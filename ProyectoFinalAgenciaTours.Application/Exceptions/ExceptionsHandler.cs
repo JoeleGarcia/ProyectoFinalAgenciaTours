f35936c98e0577942d45f1965bf023a088f26b64
JoelGarcia <Joel.eGarcia@live.com>
2025-12-06 00:40:17 -0400

    Update: custom Exception

---
+++ ProyectoFinalAgenciaTours.Application/Exceptions/ExceptionsHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAgenciaTours.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string entityName, object key)
+            : base($"La entidad \"{entityName}\" con clave ({key}) no fue encontrada.")
+        {
+        }
+    }
+
+    public class InvalidArgumentException : Exception
+    {
+        public InvalidArgumentException(string message, string param ) : base(message) { }
+    }
+
+    public class InfrastructureException : Exception
+    {
+        public InfrastructureException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+    }
+}
