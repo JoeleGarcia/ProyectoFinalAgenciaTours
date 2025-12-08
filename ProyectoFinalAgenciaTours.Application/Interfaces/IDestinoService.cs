e4a592653cd08d556ff0b1c1a7290184c8552900
JoelGarcia <Joel.eGarcia@live.com>
2025-12-08 15:02:31 -0400

    Update: Interfaces para Destino

---
+++ ProyectoFinalAgenciaTours.Application/Interfaces/IDestinoService.cs
@@ -0,0 +1,18 @@
+﻿using ProyectoFinalAgenciaTours.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAgenciaTours.Application.Interfaces
+{
+    public interface IDestinoService
+    {
+        Task<IEnumerable<DestinoDto>> GetAllDestinoAsync();
+        Task<DestinoDto> GetDestinoByIdAsync(int id);
+        Task<CreateDestinoDto> AddDestinoAsync(CreateDestinoDto createDestinoDto);
+        Task UpdateDestinoAsync(int id, UpdateDestinoDto updateDestinoDto);
+        Task DeleteDestinoAsync(int id);
+    }
+}
---
+++ ProyectoFinalAgenciaTours.Domain/Interfaces/IDestinoRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProyectoFinalAgenciaTours.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAgenciaTours.Domain.Interfaces
+{
+    public interface IDestinoRepository
+    {
+        Task<IEnumerable<Destino>> GetAllDestinosAsync();
+        Task<Destino?> GetDestinosByIdAsync(int id);
+        Task<Destino> AddDestinosAsync(Destino destinos);
+        Task UpdateDestinosAsync(Destino destinos);
+        Task DeleteDestinosAsync(int id);
+    }
+}
