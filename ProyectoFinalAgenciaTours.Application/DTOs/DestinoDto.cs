20bf541843a6a5a6fc63dac85490d08e78ac3ccf
JoelGarcia <Joel.eGarcia@live.com>
2025-12-08 15:00:26 -0400

    Update: Mejora de estructura para un mejor mapping en los IEnum

--- ProyectoFinalAgenciaTours.Application/DTOs/DestinoDto.cs
+++ ProyectoFinalAgenciaTours.Application/DTOs/DestinoDto.cs
@@ -1,6 +1,17 @@
 ﻿namespace ProyectoFinalAgenciaTours.Application.DTOs
 {
-    public record DestinoDto(int Id, string Nombre, int CapacidadMaxima, string TipoDestino, int PaisId, DateTime FechaCreacion);
-    public record CreateDestinoDto(string Nombre, int CapacidadMaxima, string TipoDestino, int PaisId);
-    public record UpdateDestinoDto(int Id, string Nombre, int CapacidadMaxima, string TipoDestino, int PaisId);
+    public record DestinoDto
+    {
+        public int Id { get; init; }
+        public string Nombre { get; init; }
+        public int CapacidadMaxima { get; init; }
+        public string TipoDestino { get; init; }
+        public int PaisId { get; init; }
+        public PaisDto Pais { get; init; }
+        public bool Status { get; set; }
+        public DateTime FechaCreacion { get; init; }
+
+    }
+    public record CreateDestinoDto(string Nombre, int CapacidadMaxima, string TipoDestino, int PaisId , bool Status);
+    public record UpdateDestinoDto(int Id, string Nombre, int CapacidadMaxima, string TipoDestino, int PaisId, bool Status);
 }
