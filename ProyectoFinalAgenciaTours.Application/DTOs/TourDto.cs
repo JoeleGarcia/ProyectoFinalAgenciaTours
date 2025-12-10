75f4004416dd0781223e01e65210a8016365e394
JoelGarcia <Joel.eGarcia@live.com>
2025-12-10 16:17:45 -0400

    Update: Modificaciones al Dto Tour

--- ProyectoFinalAgenciaTours.Application/DTOs/TourDto.cs
+++ ProyectoFinalAgenciaTours.Application/DTOs/TourDto.cs
@@ -1,26 +1,51 @@
 ﻿namespace ProyectoFinalAgenciaTours.Application.DTOs
 {
-    public record CreateTourDto(string Nombre, int PaisId, int DestinoId, DateTime FechaInicio, TimeSpan HoraInicio, decimal Precio);
-    public record UpdateTourDto(int Id, string Nombre, int PaisId, int DestinoId, DateTime FechaInicio, TimeSpan HoraInicio, decimal Precio);
-    public record TourDetailDto(int Id, string Nombre, decimal Precio, DateTime FechaInicio, TimeSpan HoraInicio, string PaisNombre, string DestinoNombre, int DuracionDiasDestino, int DuracionHorasDestino)
-    {
-        public decimal ITBIS => Precio * 0.18m;
-        public TimeSpan DuracionTotal
-        {
-            get
-            {
-                TimeSpan duration = TimeSpan.FromDays(DuracionDiasDestino);
-                return duration.Add(TimeSpan.FromHours(DuracionHorasDestino));
-            }
-        }
-        public DateTime FechaHoraFin
-        {
-            get
-            {
-                var fechaHoraInicio = FechaInicio.Date.Add(HoraInicio);
-                return fechaHoraInicio.Add(DuracionTotal);
-            }
-        }
-        public string Estado => FechaHoraFin > DateTime.UtcNow ? "Vigente" : "No Vigente";
-    }
+    public record CreateTourDto(string Nombre, int PaisId, int DestinoId, DateTime Fecha, TimeSpan Hora, int Horas, decimal TasaImpuesto, decimal Precio, decimal ITBIS, DateTime FechaFinalizacion);
+    public record UpdateTourDto(int Id, string Nombre, int PaisId, int DestinoId, DateTime Fecha, TimeSpan Hora, int Horas, decimal TasaImpuesto, decimal Precio, decimal ITBIS, DateTime FechaFinalizacion);
+    public record TourDetailDto(int Id, string Nombre, int PaisId, PaisDto Pais, int DestinoId, DestinoDto Destino, DateTime Fecha, TimeSpan Hora, int Horas, decimal TasaImpuesto, decimal Precio, decimal ITBIS, DateTime FechaFinalizacion, string Duracion, string Estado);
+
+
+
+    //public record TourDetailDto
+    //{
+    //    public int Id { get; init; }
+    //    public string Nombre { get; init; }
+
+
+
+    //    public int PaisId { get; init; }
+    //    public PaisDto Pais { get; init; }
+
+    //    public int DestinoId { get; init; }
+    //    public DestinoDto Destino { get; init; }
+
+
+
+    //    public DateTime Fecha { get; init; }
+    //    public TimeSpan Hora { get; init; }
+
+
+    //    public decimal TasaImpuesto { get; init; }
+    //    public decimal Precio { get; init; }
+
+
+    //    public decimal ITBIS { get; init; }
+    //    public DateTime FechaFializacion { get; init; }
+    //    public string Duracion { get; init; }
+    //    public string Estado { get; init; }
+
+
+
+    //    //public string PaisNombre { get; init; }
+    //    //public string DestinoNombre { get; init; }
+
+    //    //public int DuracionDiasDestino { get; init; }
+    //    //public int DuracionHorasDestino { get; init; }
+
+    //    //public decimal ITBIS { get; init; }
+
+    //    //public decimal Impuestos => Precio * (ITBIS / 100.00m);
+
+
+    //}
 }
