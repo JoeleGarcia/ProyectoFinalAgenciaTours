dadc196e05ac7cbdcec9953e048bfa7a763bac57
JoelGarcia <Joel.eGarcia@live.com>
2025-12-11 17:01:39 -0400

    Update: DependencyInjection para los Validadores

--- ProyectoFinalAgenciaTours.Application/DependencyInjection.cs
+++ ProyectoFinalAgenciaTours.Application/DependencyInjection.cs
@@ -1,7 +1,11 @@
 ﻿using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using ProyectoFinalAgenciaTours.Application.DTOs;
 using ProyectoFinalAgenciaTours.Application.Interfaces;
 using ProyectoFinalAgenciaTours.Application.Services;
+using ProyectoFinalAgenciaTours.Application.Validator;
+using SistemaCalificacion.Application.Validator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +23,20 @@ namespace ProyectoFinalAgenciaTours.Application
             services.AddScoped<IPaisService, PaisService>();
             services.AddScoped<IDestinoService, DestinoService>();
             services.AddScoped<ITourService, TourService>();
+
+            services.AddScoped<IValidator<LoginUsuarioDto>, LoginUserDtoValidator>();
+            services.AddScoped<IValidator<RegistroUsuarioDto>, RegisterUserDtoValidator>();
+            services.AddScoped<IValidator<UsuarioDto>, UsuarioDtoValidator>();
+
+            services.AddScoped<IValidator<CreateDestinoDto>, CreateDestinoDtoValidator>();
+            services.AddScoped<IValidator<UpdateDestinoDto>, UpdateDestinoDtoValidator>();
+
+            services.AddScoped<IValidator<CreatePaisDto>, CreatePaisDtoValidator>();
+            services.AddScoped<IValidator<UpdatePaisDto>, UpdatePaisDtoValidator>();
+
+            services.AddScoped<IValidator<CreateTourDto>, CreateTourDtoValidator>();
+            services.AddScoped<IValidator<UpdateTourDto>, UpdateTourDtoValidator>();
+            services.AddFluentValidationAutoValidation();
 
             services.AddAutoMapper(typeof(DependencyInjection).Assembly);
             return services;
